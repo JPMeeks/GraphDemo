b8687485c9948170fad2a4ef501b448e37329433
JP_Meeks <meeks.preston1@gmail.com>
2017-03-04 11:25:21 -0500

    Added to Git

---
+++ PE24/PE24/AdjacencyList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE24
+{
+    class AdjacencyList
+    {
+        // attributes
+        Dictionary<string, List<string>> diction; // stores names as keys, and connections as lists
+
+        // constructor
+        public AdjacencyList()
+        {
+            diction = new Dictionary<string, List<string>>();
+            List<string>[] list = new List<string>[5];
+
+            // set up lists
+            for(int x = 0; x < 5; x++)
+            {
+                list[x] = new List<string>();
+            }
+
+            // add to lists
+            list[0].Add("Kitchen");
+            list[0].Add("Parlor");
+            list[1].Add("Basement");
+            list[2].Add("Basement");
+            list[2].Add("Balcony");
+            list[2].Add("Outside");
+            list[3].Add("Parlor");
+            list[4].Add("Parlor");
+
+            // set the dictionary
+            diction.Add("Basement", list[0]);
+            diction.Add("Kitchen", list[1]);
+            diction.Add("Parlor", list[2]);
+            diction.Add("Balcony", list[3]);
+            diction.Add("Outside", list[4]);
+        }
+
+        // returns a list of adjacent rooms given a room name
+        public List<string> GetAdjacentList(string room)
+        {
+            if(diction.ContainsKey(room))
+                return diction[room];
+
+            else
+                return diction["Basement"];
+        }
+
+        // check whether two rroms are connected
+        public bool IsConnected(string room1, string room2)
+        {
+            if(GetAdjacentList(room1).Contains(room2))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ PE24/PE24/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // new adjacency list
+            AdjacencyList adjacent = new AdjacencyList();
+            string room = "Basement";
+
+            while(room != "Outside")
+            {
+                // tell current room
+                Console.WriteLine("You are in: " + room);
+
+                // get adjacent rooms
+                List<string> list = adjacent.GetAdjacentList(room);
+                Console.Write("Nearby are:");
+                for(int x = 0; x < list.Count; x++)
+                {
+                    Console.Write("   " + list[x] + "   ");
+                }
+                Console.Write("\n");
+
+                Console.Write("Go to: ");
+                string roomTemp = Console.ReadLine();
+                if(adjacent.IsConnected(room, roomTemp))
+                {
+                    room = roomTemp;
+                    if(room == "Outside")
+                    {
+                        Console.WriteLine("Congratulations! You have escaped the house!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("That room is not connected to " + room);
+                }
+            }
+        }
+    }
+}
